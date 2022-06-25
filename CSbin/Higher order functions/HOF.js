d1299c206a12111d868a8cd0544690f3c0bb37dc
RichardSchwarcz <riso.schwarcz@gmail.com>
2022-06-25 20:00:10 +0200

    .

---
+++ CSbin/Higher order functions/HOF.js
@@ -0,0 +1,171 @@
+console.log('------------------CHALLENGE 1---------------------')
+const addTwo = (num) => {
+    return num + 2;
+};
+
+// To check if you've completed this function, uncomment these console.logs!
+console.log(addTwo(3));
+console.log(addTwo(10));
+console.log('--------------------------------------------------')
+
+
+console.log('------------------CHALLENGE 2---------------------')
+const addS = (word) => {
+    return `${word}s`;
+};
+
+// Uncomment these to check your work
+console.log(addS('pizza'));
+console.log(addS('bagel'));
+console.log('--------------------------------------------------')
+
+console.log('------------------CHALLENGE 3---------------------')
+const map = (array, callback) => {
+    const newArray = [];
+    for (let element of array) {
+        newArray.push(callback(element));
+    }
+    return newArray;
+};
+
+console.log(map([1, 2, 3], addTwo));
+console.log('--------------------------------------------------')
+
+console.log('------------------CHALLENGE 4---------------------')
+const forEach = (array, callback) => {
+    for (let element of array) {
+        callback(element);
+    }
+};
+
+let alphabet = '';
+const letters = ['a', 'b', 'c', 'd'];
+forEach(letters, char => alphabet += char);
+console.log(alphabet);
+console.log('--------------------------------------------------')
+
+
+console.log('------------------CHALLENGE 5---------------------')
+const mapWith = (array, callback) => {
+    console.error( 'Not possible ??')
+};
+console.log(mapWith([1, 2, 3], addTwo));
+console.log('--------------------------------------------------')
+
+
+console.log('------------------CHALLENGE 6---------------------')
+const reduce = (array, callback, initialValue) => {
+    console.error('TODO')
+};
+
+const nums = [4, 1, 3];
+const add = (a, b) => a + b; 
+console.log(reduce(nums, add, 0));
+console.log('--------------------------------------------------')
+
+
+console.log('------------------CHALLENGE 7---------------------')
+const intersection = (arrays) => {
+    console.error('TODO')
+};
+
+console.log(intersection([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]]));
+// should log: [5, 15]
+
+
+// Challenge 8
+const union = (arrays) => {
+    
+};
+
+// console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
+// should log: [5, 10, 15, 88, 1, 7, 100]
+
+
+// Challenge 9
+const objOfMatches = (array1, array2, callback) => {
+
+};
+
+// console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], (str) => str.toUpperCase()));
+// should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
+
+
+// Challenge 10
+const multiMap = (arrVals, arrCallbacks) => {
+
+};
+
+// console.log(multiMap(['catfood', 'glue', 'beer'], [(str) => str.toUpperCase(), (str) => str[0].toUpperCase() + str.slice(1).toLowerCase(), (str) => str + str]));
+// should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+
+// Challenge 11
+const commutative = (func1, func2, value) => {
+
+};
+
+// /*** Uncomment these to check your work! ***/
+// const multBy3 = n => n * 3;
+// const divBy4 = n => n / 4;
+// const subtract5 = n => n - 5;
+// console.log(commutative(multBy3, divBy4, 11)); // should log: true
+// console.log(commutative(multBy3, subtract5, 10)); // should log: false
+// console.log(commutative(divBy4, subtract5, 48)); // should log: false
+
+
+// Challenge 12
+const objFilter = (obj, callback) => {
+
+};
+
+// /*** Uncomment these to check your work! ***/
+// const startingObj = {};
+// startingObj[6] = 3;
+// startingObj[2] = 1;
+// startingObj[12] = 4;
+// const half = n => n / 2;
+// console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
+
+
+// Challenge 13
+const rating = (arrOfFuncs, value) => {
+
+};
+
+// /*** Uncomment these to check your work! ***/
+// const isEven = n => n % 2 === 0;
+// const greaterThanFour = n => n > 4;
+// const isSquare = n => Math.sqrt(n) % 1 === 0;
+// const hasSix = n => n.toString().includes('6');
+// const checks = [isEven, greaterThanFour, isSquare, hasSix];
+// console.log(rating(checks, 64)); // should log: 100
+// console.log(rating(checks, 66)); // should log: 75
+
+
+// Challenge 14
+const pipe = (arrOfFuncs, value) => {
+
+};
+
+// /*** Uncomment these to check your work! ***/
+// const capitalize = str => str.toUpperCase();
+// const addLowerCase = str => str + str.toLowerCase();
+// const repeat = str => str + str;
+// const capAddlowRepeat = [capitalize, addLowerCase, repeat];
+// console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
+
+
+// Challenge 15
+const highestFunc = (objOfFuncs, subject) => {
+
+};
+
+// /*** Uncomment these to check your work! ***/
+// const groupOfFuncs = {};
+// groupOfFuncs.double = n => n * 2;
+// groupOfFuncs.addTen = n => n + 10;
+// groupOfFuncs.inverse = n => n * -1;
+// console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
+// console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
+// console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
